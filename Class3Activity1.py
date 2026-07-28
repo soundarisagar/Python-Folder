933247471c91ad6f007c39af37b3c45e90ddea6c
Nivedh <soundari.sagar@gmail.com>
2026-07-28 18:51:40 +0400

    Data Types

---
+++ Class3Activity1.py
@@ -0,0 +1,16 @@
+# 1) Store an integer value in `a`.
+a = 13
+# 2) Print the datatype of `a` using `type()`.
+print(type(a))
+# 3) Store a decimal (float) value in `b`.
+b = 12.06
+# 4) Print the datatype of `b` using `type()`.
+print(type(b))
+# 5) Store a text (string) value in `c`.
+c = "Hello"
+# 6) Print the datatype of `c` using `type()`.
+print(type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d = True
+# 8) Print the datatype of `d` using `type()`.
+print(type(d))
