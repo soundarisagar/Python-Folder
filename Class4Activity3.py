8475829c81dfafa10f6a21dcb8b3502e231b7b68
Nivedh <soundari.sagar@gmail.com>
2026-07-30 19:11:50 +0400

    Percentage Calculation

---
+++ Class4Activity3.py
@@ -0,0 +1,21 @@
+# 1) Ask the user to enter marks for 4 subjects: math, english, science, and hindi.
+maths = 100
+english = 100
+science = 90
+hindi = 85
+# Store each mark in its own variable.
+
+# 2) Add all 4 subject marks and store the total in `sum`.
+sum = maths + english + science + hindi
+# 3) Print the total marks stored in `sum`.
+print(sum)
+# 4) Calculate the percentage:
+perc = (sum/400) * 100
+print(perc)
+# - Divide `sum` by 400 (total maximum marks for 4 subjects, assuming each is out of 100)
+
+# - Multiply the result by 100
+
+# Store the final value in `perc`.
+
+# 5) Print the percentage stored in `perc`.
