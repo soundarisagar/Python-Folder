37b8bc65cc6ba8117186a104474c11e5819fa978
Nivedh <soundari.sagar@gmail.com>
2026-08-02 21:09:02 +0400

    Storing Birthdays

---
+++ AfterClassProject2.py
@@ -0,0 +1,7 @@
+Nivedh_DOB = "30/12/2013"
+Soundari_DOB = "12/12/2014"
+Vidhya_DOB = "18/10/2012"
+Arjun_DOB = "05/10/2013"
+Rohit_DOB = "10/06/2012"
+
+print(Nivedh_DOB, Soundari_DOB, Vidhya_DOB, Arjun_DOB, Rohit_DOB, sep="\n")
