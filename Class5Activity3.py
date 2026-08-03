65dee2229f90ca3b1171b86ba53472e52c763ea9
Nivedh <soundari.sagar@gmail.com>
2026-08-03 19:42:07 +0400

    Checking the number greater or smaller

---
+++ Class5Activity3.py
@@ -0,0 +1,10 @@
+
+num = int(input("Enter a number: "))
+
+if (num < 15):
+    print ("i is smaller than 15")
+    print ("i'm in if Block")
+
+else:
+    print ("i is greater than 15")
+    print ("i'm in else Block")
