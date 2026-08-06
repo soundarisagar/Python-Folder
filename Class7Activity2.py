51f92e384fa701877ac0e2db9a87c4ed3628aa59
Nivedh <soundari.sagar@gmail.com>
2026-08-06 18:55:13 +0400

    Electricity Bill

---
+++ Class7Activity2.py
@@ -0,0 +1,21 @@
+units = int(input("Please enter the number of units you have Consumed: "))
+amount = 0
+surcharge = 0
+if(units < 50):
+    amount = units * 2.60
+    surcharge = 25
+
+elif(units <= 100):
+    amount = 130 + ((units - 50) * 3.25)
+    surcharge = 35
+
+elif(units <= 200):
+    amount = 130 + 162.5 + ((units - 100) * 5.26)
+    surcharge = 45
+
+else:
+    amount = 130 + 162.50 + 526 + ((units - 200) * 8.45)
+    surcharge = 75
+
+total = amount + surcharge
+print("\nElectricity Bill", total)
