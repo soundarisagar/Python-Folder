e3eddeea6ae23c53f0813350161b4263ecbff0a3
Nivedh <soundari.sagar@gmail.com>
2026-08-06 18:42:31 +0400

    Exam Eligibility Check

---
+++ Class7Activity1.py
@@ -0,0 +1,12 @@
+medical_cause = input("Did you have a medical cause? (Y/N): ").strip().upper()
+
+if medical_cause == "Y":
+    print("You are allowed.")
+
+else:
+    atten = int(input("Enter the attendance of the student: "))
+
+    if atten >= 75:
+        print("You are allowed.")
+    else:
+        print("You are not allowed.")
