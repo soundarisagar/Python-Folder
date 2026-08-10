04dacd231ae3bb45d47c9491ae3fedb595c2e2ff
Nivedh <soundari.sagar@gmail.com>
2026-08-10 19:42:44 +0400

    reverse Orders

---
+++ Class8Activity3.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+print(f"Numbers from 1 to {n} are:")
+
+for i in range(n, 0, -1):
+    print(i)
---
+++ Class8Activity4.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(5):
+    dice_roll = random.randint(1, 6)
+    print("You rolled:", dice_roll)
