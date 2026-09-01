700e71e158cbeeeaa3862aecb5e80c59da682e33
Nivedh <soundari.sagar@gmail.com>
2026-09-01 18:54:04 +0400

    Break

---
+++ Class15Activity1.py
@@ -0,0 +1,18 @@
+# 1) Take a word input from the user and store it in `a`.
+a = input("Enter a word: ")
+# 2) Use a `for` loop to iterate through each character `i` in the word `a`.
+for i in a:
+    if i == 'A':
+         print("A is found")
+         break
+    else:
+        print("A is not found")
+# 3) For each character, check if it is equal to 'A':
+
+# a) If `i == 'A'`, print "A is found".
+
+# b) Use `break` to stop the loop immediately after finding 'A'.
+
+# 4) If the current character is not 'A', print "A not found".
+
+# (This message prints for each character until 'A' is found or the loop ends.)
