8b7a56e0d375e802ce56ac2cb49934c3492c86dc
Nivedh <soundari.sagar@gmail.com>
2026-09-22 21:45:23 +0400

    Square it Out

---
+++ AfterClassProject14.py
@@ -0,0 +1,16 @@
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+odd_squares = []
+even_squares = []
+
+for num in range(start, end + 1):
+    square = num ** 2
+
+    if square % 2 == 0:
+        even_squares.append(square)
+    else:
+        odd_squares.append(square)
+
+print("Odd square values:", odd_squares)
+print("Even square values:", even_squares)
