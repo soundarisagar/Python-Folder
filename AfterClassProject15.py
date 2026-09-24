51a8c6466490524a76ce6f1fec825cf8192e100e
Nivedh <soundari.sagar@gmail.com>
2026-09-24 20:53:26 +0400

    Product of tuples

---
+++ AfterClassProject15.py
@@ -0,0 +1,13 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+product1 = 1
+for num in tup1:
+    product1 *= num
+
+product2 = 1
+for num in tup2:
+    product2 *= num
+
+print("Product of tup1:", product1)
+print("Product of tup2:", product2)
